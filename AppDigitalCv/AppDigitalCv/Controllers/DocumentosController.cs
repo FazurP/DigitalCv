16d03182240e75a99181207d856d3413ebb5deaf
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2018-11-13 21:51:50 -0600

    Agregamos la vista para agregar documentos oficiales del personal.

---
+++ AppDigitalCv/AppDigitalCv/Controllers/DocumentosController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDigitalCv.Controllers
+{
+    public class DocumentosController : Controller
+    {
+        // GET: Documentos
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region  Agregar Documentos del Personal
+        /// <summary>
+        /// Esta Acción se encarga de visualizar una view con fileupload para cargar archivos
+        /// </summary>
+        /// <returns>una View con los elementos de presentación apra el usuario</returns>
+        public ActionResult AgregarDocumentos()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
--- AppDigitalCv/AppDigitalCv/ViewModels/DocumentoPersonalVM.cs
+++ AppDigitalCv/AppDigitalCv/ViewModels/DocumentoPersonalVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,5 +13,13 @@ namespace AppDigitalCv.ViewModels
         public string UrlRfc { get; set; }
         public string imgUrlCurp { get; set; }
         public string imgRfc { get; set; }
+
+        [Required(ErrorMessage = "El archivo es obligatorio")]
+        public HttpPostedFileWrapper ArchivoRfc { get; set; }
+
+        [Required(ErrorMessage = "El archivo es obligatorio")]
+        //[DataType(DataType.Upload)]
+        public HttpPostedFileWrapper ArchivoCurp { get; set; }
+
     }
 }
