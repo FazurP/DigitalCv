9500e88d1ec15303ea4f1cf538348c4638bf17da
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-01-25 00:25:30 -0600

    Funcionalidad completa  se incluyen capa de business de AsociacionesVM, PersonalAsociacionesVM y tipo de empresasVM

---
+++ AppDigitalCv/AppDigitalCv.Domain/AsociacionesDomainModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Domain
+{
+    public class AsociacionesDomainModel
+    {
+        public int IdAsociacion { get; set; }
+        public string StrDescripcion { get; set; }
+        public string StrObservacion { get; set; }
+        public int IdTipoEmpresa { get; set; }
+        public virtual TipoEmpresaDomainModel TipoEmpresaDomainModel { get; set; }
+    }
+}
