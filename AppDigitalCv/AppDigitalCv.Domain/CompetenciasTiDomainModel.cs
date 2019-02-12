897c20937e3bcfad10612cf71b4f081faa4ecf49
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-02-12 17:47:59 -0600

    Se crearon repositorio y clases domain model de el proceso de competenciasTi

---
+++ AppDigitalCv/AppDigitalCv.Domain/CompetenciasTiDomainModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Domain
+{
+    public class CompetenciasTiDomainModel
+    {
+        public int IdCompetenciaTIPersonal { get; set; }
+        public int IdCompetenciaTI { get; set; }
+        public int IdPersonal { get; set; }
+        public Nullable<System.DateTime> DteFechaRegistro { get; set; }
+
+        public virtual CompetenciaTiDomainModel CompetenciaTiDomainModel   { get; set; }
+        public virtual PersonalDomainModel PersonalDomainModel { set; get; }
+        
+    }
+}
