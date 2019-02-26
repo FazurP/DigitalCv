d347f22adf3b071eefb394a23a5e034c428b9eae
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-02-25 21:47:49 -0600

    Creación del apantalla Contacto de Emergencia, modulo create apenas se empieza a diseñar

---
+++ AppDigitalCv/AppDigitalCv/Controllers/ContactoEmergenciaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDigitalCv.Controllers
+{
+    public class ContactoEmergenciaController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
