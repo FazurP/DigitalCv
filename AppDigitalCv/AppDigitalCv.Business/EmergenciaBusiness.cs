c80b0c9964d33de91949abaa3839c1b1ad1f954d
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-02-26 17:54:45 -0600

    Modificacion de la clase EmergenciaBusiness creando los primero elementos base para las operaciones CRUD.

---
+++ AppDigitalCv/AppDigitalCv.Business/EmergenciaBusiness.cs
@@ -0,0 +1,24 @@
+﻿using AppDigitalCv.Repository;
+using AppDigitalCv.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Business
+{
+    public class EmergenciaBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly EmergenciaRepository emergenciaRepository;
+
+        public EmergenciaBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            emergenciaRepository = new EmergenciaRepository(unitOfWork);
+        }
+
+
+    }
+}
--- AppDigitalCv/AppDigitalCv.Repository/EmergenciaRepository.cs
+++ AppDigitalCv/AppDigitalCv.Repository/EmergenciaRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppDigitalCv.Repository.Infraestructure;
+using AppDigitalCv.Repository.Infraestructure.Contract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,11 @@ using System.Threading.Tasks;
 
 namespace AppDigitalCv.Repository
 {
-    public class EmergenciaRepository
+    public class EmergenciaRepository : BaseRepository<tblEmergencia>
     {
+        public EmergenciaRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
 
+        }
     }
 }
