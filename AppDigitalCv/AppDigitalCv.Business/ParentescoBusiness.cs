04e13942115d46d35631dc5caa67286f722e3982
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-02-26 20:27:50 -0600

    Modifciacion de la clase de negocio  ParentescoBusiness

---
+++ AppDigitalCv/AppDigitalCv.Business/ParentescoBusiness.cs
@@ -0,0 +1,31 @@
+﻿using AppDigitalCv.Domain;
+using AppDigitalCv.Repository;
+using AppDigitalCv.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Business
+{
+    public class ParentescoBusiness
+    {
+
+        private readonly IUnitOfWork unitOfWork;
+        private readonly ParentescoRepository parentescoRepository;
+        
+        public ParentescoBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            parentescoRepository = new ParentescoRepository(unitOfWork);
+            
+        }
+
+
+        public List<ParentescoDomainModel> GetParentescos()
+        {
+            return null;
+        }
+    }
+}
