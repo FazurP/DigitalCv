f2b9437cc7b847db1172245e4e73e253bf339a58
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-02-26 18:37:33 -0600

    Modificacion de el metodo de EmergenciasBusiness , junto con la creación de la interfaz IEmergenciaBusiness, funcionalidad aprcialmente terminada para la insercion de datos.

--- AppDigitalCv/AppDigitalCv.Business/EmergenciaBusiness.cs
+++ AppDigitalCv/AppDigitalCv.Business/EmergenciaBusiness.cs
@@ -1,4 +1,6 @@
-﻿using AppDigitalCv.Repository;
+﻿using AppDigitalCv.Business.Interface;
+using AppDigitalCv.Domain;
+using AppDigitalCv.Repository;
 using AppDigitalCv.Repository.Infraestructure.Contract;
 using System;
 using System.Collections.Generic;
@@ -8,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace AppDigitalCv.Business
 {
-    public class EmergenciaBusiness
+    public class EmergenciaBusiness : IEmergenciaBusiness
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly EmergenciaRepository emergenciaRepository;
@@ -19,6 +21,47 @@ namespace AppDigitalCv.Business
             emergenciaRepository = new EmergenciaRepository(unitOfWork);
         }
 
+        /// <summary>
+        /// este metodo sirve para agregar o editar un registro de el contexto seleccionado
+        /// </summary>
+        /// <param name="emergenciaDM">recibe una entidad del tipo de emergencia domain model</param>
+        /// <returns>una cadena de confirmación de inserción</returns>
+        public string AddUpdateEmergencia(EmergenciaDomianModel emergenciaDM)
+        {
+            string resultado = string.Empty;
+            if (emergenciaDM.IdPersonal > 0)
+            {
+                //buscamos por id y lo almacenamos en nuestra entidad de entityframework
+                tblEmergencia Emergencia = emergenciaRepository.SingleOrDefault(p=> p.idEmergencia == emergenciaDM.IdEmergencia);
+                
+                if (Emergencia != null)
+                {
+                    Emergencia.idEmergencia = emergenciaDM.IdEmergencia;
+                    Emergencia.idParentesco = emergenciaDM.IdParentesco;
+                    Emergencia.idPersonal = emergenciaDM.IdPersonal;
+                    Emergencia.strDireccion = emergenciaDM.StrDireccion;
+                    Emergencia.strNombre = emergenciaDM.StrNombre;
+                    Emergencia.strTelefono = emergenciaDM.StrTelefono;
+                    //actualizamos los datos en la base de datos.
+                    emergenciaRepository.Update(Emergencia);
+                    resultado = "Se Actualizo correctamente";
+
+                }
+            }
+            else
+            {
+                tblEmergencia tblEmergencia = new tblEmergencia();
+                tblEmergencia.idEmergencia = emergenciaDM.IdEmergencia;
+                tblEmergencia.idParentesco = emergenciaDM.IdParentesco;
+                tblEmergencia.idPersonal = emergenciaDM.IdPersonal;
+                tblEmergencia.strNombre = emergenciaDM.StrNombre;
+                tblEmergencia.strTelefono = emergenciaDM.StrTelefono;
+                tblEmergencia.strDireccion = emergenciaDM.StrDireccion;
+                emergenciaRepository.Insert(tblEmergencia);
+                resultado = "Se insertaron correctamente los valores";
+            }
+            return resultado;
+        }
 
     }
 }
---
+++ AppDigitalCv/AppDigitalCv.Business/Interface/IEmergenciaBusiness.cs
@@ -0,0 +1,19 @@
+﻿using AppDigitalCv.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Business.Interface
+{
+    public interface IEmergenciaBusiness
+    {
+        /// <summary>
+        /// este metodo sirve para agregar o editar un registro de el contexto seleccionado
+        /// </summary>
+        /// <param name="emergenciaDM">recibe una entidad del tipo de emergencia domain model</param>
+        /// <returns>una cadena de confirmación de inserción</returns>
+        string AddUpdateEmergencia(EmergenciaDomianModel emergenciaDM);
+    }
+}
