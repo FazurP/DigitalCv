215ffc3212634809b96203c70b001dcdbce77980
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-05-07 19:07:20 -0500

    Se realizo la funcionalidad de carga de alergias, registro de alergias y creacion de la pantalla.

--- AppDigitalCv/AppDigitalCv/Controllers/AlergiasController.cs
+++ AppDigitalCv/AppDigitalCv/Controllers/AlergiasController.cs
@@ -1,5 +1,7 @@
 ﻿using AppDigitalCv.Business.Interface;
+using AppDigitalCv.Domain;
 using AppDigitalCv.Security;
+using AppDigitalCv.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,22 +18,84 @@ namespace AppDigitalCv.Controllers
         {
             alergiasBusiness = _alergiasBusiness;
         }
-
+        /// <summary>
+        /// Este metodo carga los dropdawnlist al carga la vista
+        /// </summary>
+        /// <returns>una vista</returns>
         [HttpGet]
         public ActionResult Create()
         {
             if (SessionPersister.AccountSession != null)
             {
-                ViewBag.Alimentos = new SelectList(alergiasBusiness.GetAlergias(), "IdAlergia", "StrDescripcion");
-                ViewBag.Alergenos = new SelectList(alergiasBusiness.GetAlergenos(), "IdAlergia", "StrDescripcion");
-                ViewBag.Medicamentos = new SelectList(alergiasBusiness.GetMedicamentos(), "IdAlergia", "StrDescripcion");
+                ViewBag.Alimentos = new SelectList(alergiasBusiness.GetAlergias(), "IdAlergias", "strDescripcion");
+                ViewBag.Alergenos = new SelectList(alergiasBusiness.GetAlergenos(), "IdAlergias", "strDescripcion");
+                ViewBag.Medicamentos = new SelectList(alergiasBusiness.GetMedicamentos(), "IdAlergias", "strDescripcion");
                 return View();
             }
             else
             {
-               return View("~/Views/Seguridad/Login.cshtml");
+                return View("~/Views/Seguridad/Login.cshtml");
             }
+
+        }
+        /// <summary>
+        /// Este metodo se encarga de obtener los datos de la vista y los agrega a un objeto
+        /// </summary>
+        /// <param name="Alimentos"></param>
+        /// <param name="Alergenos"></param>
+        /// <param name="Medicamentos"></param>
+        /// <returns>una vista</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(string Alimentos, string Alergenos, string Medicamentos)
+        {
+            List<AlergiasPersonalVM> alergiaspersonales = new List<AlergiasPersonalVM>();
+
+            if (Alimentos != null)
+            {
+                AlergiasPersonalVM alergiasPersonalAlimentosVM = new AlergiasPersonalVM();
+                alergiasPersonalAlimentosVM.IdAlergia = int.Parse(Alimentos);
+                alergiasPersonalAlimentosVM.IdPersonal = SessionPersister.AccountSession.IdPersonal;
+                alergiasPersonalAlimentosVM.dteFechaRegistro = DateTime.Now;
+                alergiaspersonales.Add(alergiasPersonalAlimentosVM);
+
+                AlergiasPersonalVM alergiasPersonalAlergenosVM = new AlergiasPersonalVM();
+                alergiasPersonalAlergenosVM.IdAlergia = int.Parse(Alergenos);
+                alergiasPersonalAlergenosVM.IdPersonal = SessionPersister.AccountSession.IdPersonal;
+                alergiasPersonalAlergenosVM.dteFechaRegistro = DateTime.Now;
+                alergiaspersonales.Add(alergiasPersonalAlergenosVM);
+
+
+                AlergiasPersonalVM alergiasPersonalMedicamentosVM = new AlergiasPersonalVM();
+                alergiasPersonalMedicamentosVM.IdAlergia = int.Parse(Medicamentos);
+                alergiasPersonalMedicamentosVM.IdPersonal = SessionPersister.AccountSession.IdPersonal;
+                alergiasPersonalMedicamentosVM.dteFechaRegistro = DateTime.Now;
+                alergiaspersonales.Add(alergiasPersonalMedicamentosVM);
+            }
+
+            if (alergiaspersonales.Count() > 0)
+            {
+                foreach (AlergiasPersonalVM alergia in alergiaspersonales)
+                {
+                    AddPersonalAlergias(alergia);
+                }
+
+                return RedirectToAction("Create", "Alergias");
+            }
+            return View("Create");
+        }
+
+        /// <summary>
+        /// Este metodo se encarga de transportar los datos del ViewModel al DomainModel
+        /// </summary>
+        /// <param name="alergiasPersonalVM"></param>
+        /// <returns>true o false si la insersion se llevo acabo.</returns>
+        public bool AddPersonalAlergias(AlergiasPersonalVM alergiasPersonalVM)
+        {
+            AlergiasPersonalDomainModel alergiasPersonalDomainModel = new AlergiasPersonalDomainModel();
+            AutoMapper.Mapper.Map(alergiasPersonalVM, alergiasPersonalDomainModel);
             
+            return alergiasBusiness.AddUpdateAlergias(alergiasPersonalDomainModel);
         }
     }
 }
