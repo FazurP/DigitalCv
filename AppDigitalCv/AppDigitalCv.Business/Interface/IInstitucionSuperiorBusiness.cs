feb7352ac9a05dd0efc9aee14c9557afd494f164
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-05-22 11:42:57 -0500

    Se agrego la clase de isntitucion superior business con los metodos de negocio.

---
+++ AppDigitalCv/AppDigitalCv.Business/Interface/IInstitucionSuperiorBusiness.cs
@@ -0,0 +1,18 @@
+﻿using AppDigitalCv.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Business.Interface
+{
+    public interface IInstitucionSuperiorBusiness
+    {
+        /// <summary>
+        /// Este metodo se encarga de consultar todas las instituciones superiores del catalogo de la base de datos
+        /// </summary>
+        /// <returns>lista de instituciones superiores</returns>
+        List<InstitucionSuperiorDomainModel> GetInstitucionSuperior();
+    }
+}
