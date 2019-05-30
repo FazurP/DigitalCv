9272a659c616f60e31b8cd3e41901be37adabbed
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-05-30 17:52:05 -0500

    Se agregaron scripts de validacion a lña pantalla o proceso de curso, se agrego la pantalla create de curso y se modificaron las barras de acciones  del menu principal

---
+++ AppDigitalCv/AppDigitalCv/Controllers/CursoController.cs
@@ -0,0 +1,36 @@
+﻿using AppDigitalCv.Security;
+using AppDigitalCv.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDigitalCv.Controllers
+{
+    public class CursoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (SessionPersister.AccountSession != null)
+            {
+                return View();
+            }
+            else
+            {
+                return View("~/Views/Seguridad/Login.cshtml");
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StrDescripcion")] CursoVM cursoVM)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/Cursos/validate-cursos-form.js
@@ -0,0 +1,83 @@
+﻿$(document).ready(function () {
+
+    $('#strUrlDocumento').prop('disabled', true);
+    $('#IdInstitucionSuperior').prop('disabled', true);
+    $('#FechaInicio').prop('disabled', true);
+    $('#FechaTermino').prop('disabled', true);
+    $('#btnSubmit').prop('disabled', true);
+
+
+    $('#rdbSi').change(function () {
+        $('#strUrlDocumento').prop('disabled', false);
+        toastr.info("Seleccione su(s) Evidencia(s) del Curso(s) Impartido.", "Digital-Cv dice", { timeOut: 1000, closeButton: true })
+    })
+
+    $('#rdbNo').change(function () {
+        $('#strUrlDocumento').prop('disabled', true);
+               
+    })
+
+    $('#Cursos').change(function () {
+
+        var curso = $('#Cursos').val();
+
+        if (curso == null || curso == 0 || curso == '0' || curso == "0") {
+
+            $('#IdInstitucionSuperior').prop('disabled', true);
+            $('#FechaInicio').prop('disabled', true);
+            $('#FechaTermino').prop('disabled', true);
+            
+            $('#btnSubmit').prop('disabled', true);
+
+            $('#IdInstitucionSuperior').val(0);
+            $('#FechaInicio').val('');
+            $('#FechaTermino').val('');
+
+        } else {
+            $('#IdInstitucionSuperior').prop('disabled', false);
+            toastr.success('Institución Seleccionado', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+
+    $('#IdInstitucionSuperior').change(function () {
+
+        var institucion = $('#IdInstitucionSuperior').val();
+
+        if (institucion == null || institucion == 0 || institucion == '0' || institucion == "0") {
+            $('#FechaInicio').prop('disabled', true);
+            $('#FechaTermino').prop('disabled', true);
+            $('#btnSubmit').prop('disabled', true);
+        } else {
+            $('#FechaInicio').prop('disabled', false);
+            toastr.success('Institución Selecccionada', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#FechaInicio').change(function () {
+        var fechaInicio = $('#FechaInicio').val();
+        if (fechaInicio == null || fechaInicio == 0 || fechaInicio == '0' || fechaInicio == "0")
+        {
+            $('#FechaTermino').prop('disabled', true);
+            $('#btnSubmit').prop('disabled', true);
+        }
+        else {
+            $('#FechaTermino').prop('disabled', false);
+            toastr.success('Fecha de Inicio Seleccionada', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#FechaTermino').change(function () {
+        var fechaTermino = $('#FechaTermino').val();
+        if (fechaTermino == null || fechaTermino == 0 || fechaTermino == '0' || fechaTermino == "0")
+        {
+            $('#btnSubmit').prop('disabled', true);
+        }
+        else {
+            $('#btnSubmit').prop('disabled', false);
+            toastr.success('Fecha de Termino Seleccionada', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    
+})
