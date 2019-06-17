5a4e2fe72e6a070388bc0daca22b2580dedaf215
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-06-17 18:46:13 -0500

    Cracion parcial de la pantalla ExperienciaLaboralInterna/Create

--- AppDigitalCv/AppDigitalCv/Controllers/ExperienciaLaboralInternaController.cs
+++ AppDigitalCv/AppDigitalCv/Controllers/ExperienciaLaboralInternaController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AppDigitalCv.Business.Interface;
+using AppDigitalCv.Domain;
+using AppDigitalCv.Security;
+using AppDigitalCv.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +13,46 @@ namespace AppDigitalCv.Controllers
     public class ExperienciaLaboralInternaController : Controller
     {
 
-        public ExperienciaLaboralInternaController() { }
+        IAreaBusiness areaBusiness;
+        IProgramaEducativoBusiness programaEducativoBusiness;
+        ITipoContratoBusiness tipoContratoBusiness;
+        ICategoriaBusiness categoriaBusiness;
+        IPeriodoBusiness periodoBusiness;
+
+        public ExperienciaLaboralInternaController(IAreaBusiness _areaBusiness, IProgramaEducativoBusiness _programaEducativoBusiness,
+            ITipoContratoBusiness _tipoContratoBusiness, ICategoriaBusiness _categoriaBusiness, IPeriodoBusiness _periodoBusiness)
+        {
+            areaBusiness = _areaBusiness;
+            programaEducativoBusiness = _programaEducativoBusiness;
+            tipoContratoBusiness = _tipoContratoBusiness;
+            categoriaBusiness = _categoriaBusiness;
+            periodoBusiness = _periodoBusiness;
+        }
         public ActionResult Create()
         {
-            return View();
+            if (SessionPersister.AccountSession != null)
+            {
+                ViewBag.idCategoria = new SelectList(categoriaBusiness.GetCategorias(), "idCategoria", "strDescripcion");
+                ViewBag.idTipoContrato = new SelectList("");
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login","Seguridad");
+            }
+           
+        }
+
+        [HttpPost]
+        public ActionResult GetTipoContratoByCategoria(int idCategoria)
+        {
+            List<TipoContratoDomainModel> tipoContrato = tipoContratoBusiness.GetTiposContratoById(idCategoria);
+            List<TipoContratoVM> tipoContratoVM = new List<TipoContratoVM>();
+
+            AutoMapper.Mapper.Map(tipoContrato,tipoContratoVM);
+            ViewBag.idTipoContrato = new SelectList(tipoContratoVM,"idTipoContrato","strDescripcion");
+            return PartialView("_TipoContrato");
         }
     }
 }
