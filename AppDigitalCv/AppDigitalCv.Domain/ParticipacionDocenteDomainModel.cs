8fdf9e955cf84ffd1235ee472d00b5ed1d7bc052
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-06-18 15:13:38 -0500

    Se crearon los domain model y viewmodel de la participacion docente.

---
+++ AppDigitalCv/AppDigitalCv.Domain/ParticipacionDocenteDomainModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Domain
+{
+    public class ParticipacionDocenteDomainModel
+    {
+        public int Id { get; set; }
+        public int IdCatDocumento { get; set; }
+        public int IdPersonal { get; set; }
+        public int StrEvento { get; set; }
+        public int StrTipoEvento { get; set; }
+        public int StrParticipacion { get; set; }
+        public int StrTipoParticipacion { get; set; }
+        public bool BitPonencia { get; set; }
+        public string StrNombrePonencia { get; set; }
+        public string StrNombreInstitucionEmpresa { get; set; }
+        public string StrLugar { get; set; }
+        public int DteFecha { get; set; }
+
+
+    }
+}
---
+++ AppDigitalCv/AppDigitalCv/ViewModels/ParticipacionDocenteVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDigitalCv.ViewModels
+{
+    public class ParticipacionDocenteVM
+    {
+        public int Id { get; set; }
+        public int IdCatDocumento { get; set; }
+        public int IdPersonal { get; set; }
+        public int StrEvento { get; set; }
+        public int StrTipoEvento { get; set; }
+        public int StrParticipacion { get; set; }
+        public int StrTipoParticipacion { get; set; }
+        public bool BitPonencia { get; set; }
+        public string StrNombrePonencia { get; set; }
+        public string StrNombreInstitucionEmpresa { get; set; }
+        public string StrLugar { get; set; }
+        public int DteFecha { get; set; }
+    }
+}
