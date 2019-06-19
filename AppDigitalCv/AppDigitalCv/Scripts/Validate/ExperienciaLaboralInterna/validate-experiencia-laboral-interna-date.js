d4f827da13314fc71ae5e0219dd9337b7cc5a7d4
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-06-18 19:59:25 -0500

    Validacion de formulario y fecha del modulo ExperienciaLaboralInterna

--- AppDigitalCv/AppDigitalCv/Scripts/Validate/ExperienciaLaboralInterna/validate-experiencia-laboral-interna-date.js
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/ExperienciaLaboralInterna/validate-experiencia-laboral-interna-date.js
@@ -1,1 +1,61 @@
-﻿
+﻿$(document).ready(function () {
+
+    dateStart = "";
+    dateEnd = "";
+
+    var dateNow = new Date();
+
+    $('#dteFechaInicio').change(function () {
+
+        dateStart = $(this).val();
+
+        var day;
+        var month;
+        var year;
+
+        year = dateNow.getFullYear();
+        month = dateNow.getMonth() + 1;
+        day = dateNow.getDate();
+
+        var dateCurrent = year + '-' + 0 + month + '-' + day;
+
+        if (dateStart > dateCurrent) {
+            toastr.warning('La Fecha de Inicio no Puede ser Mayor a la Actual', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $(this).val('');
+            $('#dteFechaTermino').prop('disabled', true);
+            return false;
+        }
+
+    });
+
+    $('#dteFechaTermino').change(function () {
+
+        dateEnd = $(this).val();
+
+
+        var day;
+        var month;
+        var year;
+
+        year = dateNow.getFullYear();
+        month = dateNow.getMonth() + 1;
+        day = dateNow.getDate();
+
+        var dateCurrent = year + '-' + 0 + month + '-' + day;
+
+        if (dateEnd < dateStart) {
+            toastr.warning('La Fecha de Terminación no Puede ser Anterior', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $(this).val('');
+            $('#txtActividades').prop('disabled', true);
+            return false;
+        }
+        if (dateCurrent < dateEnd) {
+            toastr.warning('La Fecha de Terminación no Puede ser una Fecha Superior al Dia Actual', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $(this).val('');
+            $('#txtActividades').prop('disabled', true);
+            return false;
+        }
+
+    })
+
+});
--- AppDigitalCv/AppDigitalCv/Scripts/Validate/ExperienciaLaboralInterna/validate-experiencia-laboral-interna-form.js
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/ExperienciaLaboralInterna/validate-experiencia-laboral-interna-form.js
@@ -1,1 +1,183 @@
-﻿
+﻿$(document).ready(function () {
+
+    $('#idTipoContrato').prop('disabled', true);
+    $('#idArea').prop('disabled', true);
+    $('#idProgramaEducativo').prop('disabled', true);
+    $('#idPeriodo').prop('disabled', true);
+    $('#dteFechaInicio').prop('disabled', true);
+    $('#dteFechaTermino').prop('disabled', true);
+    $('#strActividadDesempeñada').prop('disabled', true);
+    $('#btnGuardar').prop('disabled', true);
+
+    $('#idCategoria').change(function () {
+
+        var dato = $(this).val();
+
+        if (dato == null || dato == 0 || dato == '0' || dato == "0") {
+            $('#idTipoContrato').prop('disabled', true);
+            $('#idArea').prop('disabled', true);
+            $('#idProgramaEducativo').prop('disabled', true);
+            $('#idPeriodo').prop('disabled', true);
+            $('#dteFechaInicio').prop('disabled', true);
+            $('#dteFechaTermino').prop('disabled', true);
+            $('#strActividadDesempeñada').prop('disabled', true);
+            $('#btnGuardar').prop('disabled', true);
+
+            $('#idTipoContrato').val(0);
+            $('#idArea').val(0);
+            $('#idProgramaEducativo').val(0);
+            $('#idPeriodo').val(0);
+            $('#dteFechaInicio').val('');
+            $('#dteFechaTermino').val('');
+            $('#strActividadDesempeñada').val('');
+        } else {
+            toastr.success('Categoria Seleccionada', 'Digital-Cv dice', { timeOut: 1000, closeButton:true });
+            $('#idTipoContrato').prop('disabled', false);
+        }
+
+    })
+
+    $('#idTipoContrato').change(function () {
+
+        var dato = $(this).val();
+
+        if (dato == null || dato == 0 || dato == '0' || dato == "0") {
+            $('#idArea').prop('disabled', true);
+            $('#idProgramaEducativo').prop('disabled', true);
+            $('#idPeriodo').prop('disabled', true);
+            $('#dteFechaInicio').prop('disabled', true);
+            $('#dteFechaTermino').prop('disabled', true);
+            $('#strActividadDesempeñada').prop('disabled', true);
+            $('#btnGuardar').prop('disabled', true);
+
+            $('#idArea').val(0);
+            $('#idProgramaEducativo').val(0);
+            $('#idPeriodo').val(0);
+            $('#dteFechaInicio').val('');
+            $('#dteFechaTermino').val('');
+            $('#strActividadDesempeñada').val('');
+        } else {
+            toastr.success('Tipo de Contrato Seleccionado', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $('#idArea').prop('disabled', false);
+        }
+
+    })
+
+    $('#idArea').change(function () {
+
+        var dato = $(this).val();
+
+        if (dato == null || dato == 0 || dato == '0' || dato == "0") {
+            $('#idProgramaEducativo').prop('disabled', true);
+            $('#idPeriodo').prop('disabled', true);
+            $('#dteFechaInicio').prop('disabled', true);
+            $('#dteFechaTermino').prop('disabled', true);
+            $('#strActividadDesempeñada').prop('disabled', true);
+            $('#btnGuardar').prop('disabled', true);
+
+            $('#idProgramaEducativo').val(0);
+            $('#idPeriodo').val(0);
+            $('#dteFechaInicio').val('');
+            $('#dteFechaTermino').val('');
+            $('#strActividadDesempeñada').val('');
+        } else {
+            toastr.success('Area Seleccionada', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $('#idProgramaEducativo').prop('disabled', false);
+        }
+
+    })
+
+    $('#idProgramaEducativo').change(function () {
+
+        var dato = $(this).val();
+
+        if (dato == null || dato == 0 || dato == '0' || dato == "0") {
+
+            $('#idPeriodo').prop('disabled', true);
+            $('#dteFechaInicio').prop('disabled', true);
+            $('#dteFechaTermino').prop('disabled', true);
+            $('#strActividadDesempeñada').prop('disabled', true);
+            $('#btnGuardar').prop('disabled', true);
+
+            $('#idPeriodo').val(0);
+            $('#dteFechaInicio').val('');
+            $('#dteFechaTermino').val('');
+            $('#strActividadDesempeñada').val('');
+        } else {
+            toastr.success('Programa Educativo Seleccionado', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $('#idPeriodo').prop('disabled', false);
+        }
+
+    })
+
+    $('#idPeriodo').change(function () {
+
+        var dato = $(this).val();
+
+        if (dato == null || dato == 0 || dato == '0' || dato == "0") {
+
+            $('#dteFechaInicio').prop('disabled', true);
+            $('#dteFechaTermino').prop('disabled', true);
+            $('#strActividadDesempeñada').prop('disabled', true);
+            $('#btnGuardar').prop('disabled', true);
+
+            $('#dteFechaInicio').val('');
+            $('#dteFechaTermino').val('');
+            $('#strActividadDesempeñada').val('');
+        } else {
+            toastr.success('Periodo Seleccionado', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $('#dteFechaInicio').prop('disabled', false);
+        }
+
+    })
+
+    $('#dteFechaInicio').change(function () {
+
+        var dato = $(this).val();
+
+        if (dato == null || dato == 0 || dato == '0' || dato == "0") {
+
+            $('#dteFechaTermino').prop('disabled', true);
+            $('#strActividadDesempeñada').prop('disabled', true);
+            $('#btnGuardar').prop('disabled', true);
+
+            $('#dteFechaTermino').val('');
+            $('#strActividadDesempeñada').val('');
+        } else {
+
+            $('#dteFechaTermino').prop('disabled', false);
+        }
+
+    })
+
+    $('#dteFechaTermino').change(function () {
+
+        var dato = $(this).val();
+
+        if (dato == null || dato == 0 || dato == '0' || dato == "0") {
+
+            $('#strActividadDesempeñada').prop('disabled', true);
+            $('#btnGuardar').prop('disabled', true);
+
+            $('#strActividadDesempeñada').val('');
+        } else {
+            $('#strActividadDesempeñada').prop('disabled', false);
+        }
+
+    })
+
+    $('#strActividadDesempeñada').keyup(function () {
+
+        var dato = $(this).val();
+
+        if (dato == null || dato == '' || dato == "") {
+
+            $('#btnGuardar').prop('disabled', true);
+
+        } else {
+            $('#btnGuardar').prop('disabled', false);
+        }
+
+    })
+
+})
