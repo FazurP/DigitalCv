afd4e966d502e4fec0941c3d0f0cb2e7b48f2b38
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-06-26 13:05:45 -0500

    Se creo el script de validacion de participacion docente, asi como el metodo create sin funcionalidad del controlador del modulo.

--- AppDigitalCv/AppDigitalCv/Scripts/Validate/ParticipacionDocente/validate-participacion-docente-form.js
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/ParticipacionDocente/validate-participacion-docente-form.js
@@ -2,6 +2,13 @@
 
     $('#StrTipoEvento').prop('disabled', true);
     $('#StrParticipacion').prop('disabled', true);
+    $('input[id=si]').prop('disabled', true);
+    $('#StrNombrePonencia').prop('disabled', true);
+    $('#StrNombreInstitucionEmpresa').prop('disabled', true);
+    $('#StrLugar').prop('disabled', true);
+    $('#DteFecha').prop('disabled', true);
+
+
 
     $('#StrEvento').keyup(function () {
 
@@ -31,6 +38,84 @@
 
 
     $('#StrParticipacion').keyup(function () {
+
+        var participacion = $('#StrParticipacion').val();
+        if (participacion == null || participacion == '' || participacion == "") {
+            $('#StrNombrePonencia').prop('disabled', true);
+            $('input[id=si]').prop('disabled', true);
+        }
+        else
+        {
+            $('input[id=si]').prop('disabled', false);
+        }
+    })
+    $('input[id=si]').change(function () {
+
+        let si = $('input[id=si]:checked').val();
+        if (si == null || si == '' || si == "") {
+            $('#StrNombrePonencia').prop('disabled', true);
+            $('input[id=si]').prop('disabled', true);
+        }
+        else
+        {
+            $('#StrNombrePonencia').prop('disabled', false);
+        }
+
+    })
+
+    $('#StrNombrePonencia').keyup(function () {
+
+        var nombrePonencia = $('#StrNombrePonencia').val();
+        if (nombrePonencia == null || nombrePonencia == '' || nombrePonencia == "") {
+            
+            $('#StrNombreInstitucionEmpresa').prop('disabled', true);
+        }
+        else
+        {
+            $('#StrNombrePonencia').prop('disabled', false);
+            $('#StrNombreInstitucionEmpresa').prop('disabled', false);
+        }
+
+    })
+
+    $('#StrNombreInstitucionEmpresa').keyup(function () {
+
+        var institucion = $('#StrNombreInstitucionEmpresa').val();
+        if (institucion == null || institucion == '' || institucion == "") {
+            
+            $('#StrLugar').prop('disabled', true);
+        }
+        else {
+            $('#StrNombreInstitucionEmpresa').prop('disabled', false);
+            $('#StrLugar').prop('disabled', false);
+        }
+    })
+    $('#StrLugar').keyup(function () {
+
+        var lugar = $('#StrLugar').val();
+        if (lugar == null || lugar == '' || lugar == "") {
+            
+            $('#DteFecha').prop('disabled', true);
+        }
+        else
+        {
+            $('#StrLugar').prop('disabled', false);
+            $('#DteFecha').prop('disabled', false);
+        }
+
+
+    })
+
+    $('#DteFecha').change(function () {
+        let fecha = $('#DteFecha').val();
+        if (fecha == '' || fecha == "" || fecha == null)
+        {
+            $('#DteFecha').prop('disabled', true);
+
+        }
+        else {
+            $('#DteFecha').prop('disabled', false);
+        }
 
 
     })
