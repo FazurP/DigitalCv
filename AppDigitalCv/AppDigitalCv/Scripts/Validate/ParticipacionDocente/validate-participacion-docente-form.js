42d1f25f2f057108a6400bd41ff8fe6181fb97d1
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-06-25 21:46:56 -0500

    Se creo el script de validacion del modulo de participacion docente, aun no esta funcional, solo valida los primeros tres campos.

---
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/ParticipacionDocente/validate-participacion-docente-form.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+
+    $('#StrTipoEvento').prop('disabled', true);
+    $('#StrParticipacion').prop('disabled', true);
+
+    $('#StrEvento').keyup(function () {
+
+        var evento = $('#StrEvento').val();
+
+        if (evento == null || evento == '' || evento == "") {
+
+            $('#StrTipoEvento').prop('disabled', true);
+        } else {
+            $('#StrTipoEvento').prop('disabled', false);
+        }
+
+    })
+
+    $('#StrTipoEvento').keyup(function () {
+
+        var tipoEvento = $('#StrTipoEvento').val();
+
+        if (tipoEvento == null || tipoEvento == '' || tipoEvento == "") {
+            $('#StrParticipacion').prop('disabled', true);
+        }
+        else
+        {
+            $('#StrParticipacion').prop('disabled', false);
+        }
+    })
+
+
+    $('#StrParticipacion').keyup(function () {
+
+
+    })
+
+
+});
