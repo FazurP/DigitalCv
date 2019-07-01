d05d8884623d5d30a47f7a78aeace44003e8b43b
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2019-07-01 16:54:12 -0500

    modsificacion de la pantalla participacion docente, se agrego el script  valdate-participacion-docente, se creo  la pantalla create del modulo de participacion docente

--- AppDigitalCv/AppDigitalCv/ViewModels/ParticipacionDocenteVM.cs
+++ AppDigitalCv/AppDigitalCv/ViewModels/ParticipacionDocenteVM.cs
@@ -11,16 +11,25 @@ namespace AppDigitalCv.ViewModels
         public int Id { get; set; }
         public int IdCatDocumento { get; set; }
         public int IdPersonal { get; set; }
+
         [Required(ErrorMessage = "El Nombre del Evento es Obligatorio")]
         public string StrEvento { get; set; }
+
         [Required(ErrorMessage = "El tipo de Evento es Obligatorio")]
         public string StrTipoEvento { get; set; }
+
         public string StrParticipacion { get; set; }
+
         public string StrTipoParticipacion { get; set; }
+
         public bool BitPonencia { get; set; }
+
         public string StrNombrePonencia { get; set; }
+
         public string StrNombreInstitucionEmpresa { get; set; }
+
         public string StrLugar { get; set; }
+
         public string DteFecha { get; set; }
 
         public virtual DocumentosVM CatDocumentosVM{ get; set; }
