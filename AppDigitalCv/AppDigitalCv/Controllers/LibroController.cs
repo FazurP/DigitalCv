54b09afcd10715dda0ed00b5c5b0d59f38fe6b91
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-02 14:09:46 -0500

    Creacion del controlador de tblLibro.

---
+++ AppDigitalCv/AppDigitalCv/Controllers/LibroController.cs
@@ -0,0 +1,38 @@
+﻿using AppDigitalCv.Business.Enum;
+using AppDigitalCv.Business.Interface;
+using AppDigitalCv.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDigitalCv.Controllers
+{
+    public class LibroController : Controller
+    {
+        ILibroBusiness libroBusiness;
+        IPaisBusiness paisBusiness;
+        List list = new List();
+
+        public LibroController(ILibroBusiness _libroBusiness, IPaisBusiness _paisBusiness)
+        {
+            libroBusiness = _libroBusiness;
+            paisBusiness = _paisBusiness;
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (SessionPersister.AccountSession != null)
+            {
+                return View();
+            }
+            else {
+                return RedirectToAction("Login","Seguridad");
+            }
+         
+        }
+
+    }
+}
