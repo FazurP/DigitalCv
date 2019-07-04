23a6293ed5a0222475773ad6e0160ccd91d77aa4
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-04 11:40:59 -0500

    Creacion del controlador de la entidad tblManualOperacion

---
+++ AppDigitalCv/AppDigitalCv/Controllers/ManualOperacionController.cs
@@ -0,0 +1,28 @@
+﻿using AppDigitalCv.Business.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDigitalCv.Controllers
+{
+    public class ManualOperacionController : Controller
+    {
+        IManualOperacionBusiness operacionBusiness;
+        IPaisBusiness paisBusiness;
+
+        public ManualOperacionController(IManualOperacionBusiness _operacionBusiness,IPaisBusiness _paisBusiness)
+        {
+
+            operacionBusiness = _operacionBusiness;
+            paisBusiness = _paisBusiness;
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
