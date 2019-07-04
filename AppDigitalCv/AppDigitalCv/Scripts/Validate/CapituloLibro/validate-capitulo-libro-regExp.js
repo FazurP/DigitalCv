0d75e235962399e9e19a3135521398ef62048b43
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-04 09:47:37 -0500

    Validacion del lado del cliente con expresiones regulares en el modulo CapituloLibro

---
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/CapituloLibro/validate-capitulo-libro-regExp.js
@@ -0,0 +1,119 @@
+﻿$(document).ready(function () {
+
+    $('#Autor').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#Titulo').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+
+    $('#Editorial').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ0-9. ]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+
+    $('#Edicion').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ0-9. ]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+
+    $('#Tiraje').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[0-9]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Numeros', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#ISBN').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[0-9]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Numeros', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#TituloCapitulo').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+
+    $('#Autores').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+
+    $('#PaginaInicio').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[0-9]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Numeros', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+
+    $('#PaginaFin').keypress(function (e) {
+
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[0-9]+$");
+
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Numeros', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+})
