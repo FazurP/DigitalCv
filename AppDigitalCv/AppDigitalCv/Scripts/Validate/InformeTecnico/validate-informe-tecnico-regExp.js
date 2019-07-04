67e3cf685fa02146f01c40d792a9f663a8f3cedd
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-04 09:19:43 -0500

    Validacion del lado del cliente con expresiones regulares

---
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/InformeTecnico/validate-informe-tecnico-regExp.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function () {
+
+    $('#Autor').keypress(function (e) {
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+        if (!regExp.test(data)) {
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            e.preventDefault();
+        }
+    })
+
+    $('#NombreProyecto').keypress(function (e) {
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#Alcance').keypress(function (e) {
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#Institucion').keypress(function (e) {
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#NumeroPaginas').keypress(function (e) {
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[0-9]+$");
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Numeros', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+
+    $('#Proposito').keypress(function (e) {
+        let data = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        let regExp = new RegExp("^[a-záéíóúñA-ZÁÉÍÓÚÑ. ]+$");
+        if (!regExp.test(data)) {
+            e.preventDefault();
+            toastr.warning('Solo se Permiten Cadenas de Texto', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+    })
+})
