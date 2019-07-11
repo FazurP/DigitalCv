dc1780d19cbd69af5c7ede302a3f6faccf7902d4
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-11 15:13:51 -0500

    Validacion de fechas y carga de archivos en el modulo ProduccionesArtisticas

---
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/ProduccionesArtisticas/validate-producciones-artisticas-upload.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    $('#inputFileUpload').change(function () {
+
+        let data = $(this).val();
+
+        let regExp = /.pdf/;
+
+        if (!regExp.exec(data)) {
+            $('#btnGuardar').prop('disabled', true);
+            toastr.warning('Solo Se Permiten Archivos PDF', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $(this).val('');
+        } else
+        {
+            toastr.success('Archivo Cargado Correctamente', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+        }
+
+    })
+
+})
