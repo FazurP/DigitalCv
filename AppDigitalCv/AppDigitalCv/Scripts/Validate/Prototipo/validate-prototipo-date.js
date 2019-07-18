d9fa0ee7f37d2e887e688ba774811afaa52d7ad8
fazur alejandro estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-18 14:55:43 -0500

    validacion de fechas y archivos del lado del cliente, en el modulo Prototipo/Create

---
+++ AppDigitalCv/AppDigitalCv/Scripts/Validate/Prototipo/validate-prototipo-date.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    $('#dteFechaPublicacion').change(function () {
+
+        let registerDate = $(this).val();
+
+        let dateNow = new Date();
+
+        let day;
+        let month;
+        let year;
+
+        year = dateNow.getFullYear();
+        month = dateNow.getMonth() + 1;
+        day = dateNow.getDate();
+
+        let currentDate = year + '-' + 0 + month + '-' + day;
+
+        if (registerDate > currentDate) {
+            toastr.warning('La Fecha de Registro no Puede ser Mayor a la Actual', 'Digital-Cv dice', { timeOut: 1000, closeButton: true });
+            $(this).val('');
+            $('#ddlEstadoActual').prop('disabled', true);
+            return false;
+        }
+    })
+
+})
