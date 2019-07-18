47653759d0e6978d4501ecc3f4e9f98d86288301
fazur alejandro estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-18 15:50:15 -0500

    Creacion del ViewModel de la entidad tblProyectoInvestigacionAplicadaDesarrolloTecnologico

---
+++ AppDigitalCv/AppDigitalCv/ViewModels/ProyectoInvestigacionVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDigitalCv.ViewModels
+{
+    public class ProyectoInvestigacionVM
+    {
+        public int id { get; set; }
+        public int idDocumento { get; set; }
+        public int idStatus { get; set; }
+        public int idPersonal { get; set; }
+        public DocumentosVM documentosVMReseña { get; set; }
+        public DocumentosVM documentosVMResumen { get; set; }
+        public string strTituloProyecto { get; set; }
+        public string strNombrePatrocinador { get; set; }
+        public DateTime dteFechaInicio { get; set; }
+        public DateTime dteFechaTermino { get; set; }
+        public string strTipoPatrocinador { get; set; }
+        public string strInvestigadoresParticipantes { get; set; }
+        public string strAlumnosParticipantes { get; set; }
+        public string strActividadesRealizadas { get; set; }
+        public string strConvocatoria { get; set; }
+        public bool bitProyectoTecnologico { get; set; }
+        public bool bitConsideraCurriculum { get; set; }
+    }
+}
