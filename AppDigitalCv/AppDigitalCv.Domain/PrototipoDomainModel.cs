b21820a8b2e92a6bc72746630e6100ce6b58775c
fazur alejandro estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-17 19:38:12 -0500

    Se Creo el DomainModel de la   Entidad tblPrototipo

---
+++ AppDigitalCv/AppDigitalCv.Domain/PrototipoDomainModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Domain
+{
+    public class PrototipoDomainModel
+    {
+        public int id { get; set; }
+        public int idPais { get; set; }
+        public int idDocumento { get; set; }
+        public int idPersonal { get; set; }
+        public int idStatsu { get; set; }
+        public string strAutor { get; set; }
+        public string strTipoPrototipo { get; set; }
+        public string strNombrePrototipo { get; set; }
+        public string strObjetivos { get; set; }
+        public string strCaracteristicas { get; set; }
+        public string strInstitucionDestinada { get; set; }
+        public DateTime dteFechaPublicacion { get; set; }
+        public string strEstadoActual { get; set; }
+        public string strProposito { get; set; }
+        public bool bitConsideraCurriculum { get; set; }
+    }
+}
