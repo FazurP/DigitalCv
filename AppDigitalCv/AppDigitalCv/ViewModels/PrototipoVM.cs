12d6b7f773bcbbee44f0f121d75bb703ce8b5a7a
fazur alejandro estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-17 19:39:57 -0500

    Se Creo el ViewModel de la Entidad tblPrototipo

---
+++ AppDigitalCv/AppDigitalCv/ViewModels/PrototipoVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDigitalCv.ViewModels
+{
+    public class PrototipoVM
+    {
+        public int id { get; set; }
+        public int idPais { get; set; }
+        public int idDocumento { get; set; }
+        public int idPersonal { get; set; }
+        public int idStatsu { get; set; }
+        public string strAutor { get; set; }
+        public string strTipoPrototipo { get; set; }
+        public string strNombrePrototipo { get; set; }
+        public string strObjetivos { get; set; }
+        public string strCaracteristicas { get; set; }
+        public string strInstitucionDestinada { get; set; }
+        public DateTime dteFechaPublicacion { get; set; }
+        public string strEstadoActual { get; set; }
+        public string strProposito { get; set; }
+        public bool bitConsideraCurriculum { get; set; }
+    }
+}
