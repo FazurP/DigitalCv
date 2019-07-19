85a8a2008437fcbdbeab28e37ebdba1742c6ea8c
fazur alejandro estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-19 12:16:04 -0500

    se agrego el DomainModel de la entidad tblDireccionIndividualizada

---
+++ AppDigitalCv/AppDigitalCv.Domain/DireccionIndividualizadaDomainModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Domain
+{
+    public class DireccionIndividualizadaDomainModel
+    {
+        public int id { get; set; }
+        public int idPersonal { get; set; }
+        public int idTipoEstudio { get; set; }
+        public int idInstitucionSuperior { get; set; }
+        public string strTituloTesis { get; set; }
+        public DateTime dteFechaInicio { get; set; }
+        public DateTime dteFechaTermino { get; set; }
+        public string strNumeroAlumnos { get; set; }
+        public string strEstadoDireccionIndividualizada { get; set; }
+        public bool bitConsideraCurriculum { get; set; }
+    }
+}
