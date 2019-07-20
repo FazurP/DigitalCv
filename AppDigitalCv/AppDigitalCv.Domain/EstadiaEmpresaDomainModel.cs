433ec1fd02b13b7187f68206063f75f5578697b3
fazur alejandro estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-20 14:27:21 -0500

    Se creo el DomainModel de las entidades tblEstadiaEmpresa y catTipoProducto

---
+++ AppDigitalCv/AppDigitalCv.Domain/EstadiaEmpresaDomainModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Domain
+{
+    public class EstadiaEmpresaDomainModel
+    {
+        public int id { get; set; }
+        public int idTipoProducto { get; set; }
+        public int idProgramaEducativo { get; set; }
+        public int idPersonal { get; set; }
+        public int idStatus { get; set; }
+        public int idDocumento { get; set; }
+        public string strNombreEstadia { get; set; }
+        public string strResumenProyecto { get; set; }
+        public string strObjetivo { get; set; }
+        public DateTime dteFechaInicio { get; set; }
+        public DateTime dteFechaTermino { get; set; }
+        public string strNumeroAlumnos { get; set; }
+        public string strNumeroHoras { get; set; }
+        public string strNombreEmpresaInstitucion { get; set; }
+        public string strPuntosCriticosResolver { get; set; }
+        public string strLogrosBeneficiosObtenidos { get; set; }
+        public string strEstadoEstadia { get; set; }
+        public bool bitConsideraCurriculum { get; set; }
+    }
+}
