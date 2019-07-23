885c0ee1c7e9f978c59fc043617bd0aeb2a05168
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-23 12:42:17 -0500

    funcionalidad de insercion de datos, en el modulo EstadiaEmpresa/Create

--- AppDigitalCv/AppDigitalCv/Controllers/EstadiaEmpresaController.cs
+++ AppDigitalCv/AppDigitalCv/Controllers/EstadiaEmpresaController.cs
@@ -1,9 +1,12 @@
 ﻿using AppDigitalCv.Business.Enum;
 using AppDigitalCv.Business.Interface;
+using AppDigitalCv.Domain;
 using AppDigitalCv.Repository.Infraestructure.Contract;
 using AppDigitalCv.Security;
+using AppDigitalCv.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -44,6 +47,65 @@ namespace AppDigitalCv.Controllers
                 return RedirectToAction("Login","Seguridad");
             }
             
+        }
+
+        [HttpPost]
+        public ActionResult Create(EstadiaEmpresaVM estadiaEmpresaVM)
+        {
+            if (ModelState.IsValid)
+            {
+                EstadiaEmpresaDomainModel estadiaEmpresaDM = new EstadiaEmpresaDomainModel();
+                ProgresoProdepDomainModel progresoProdepDM = new ProgresoProdepDomainModel();
+                DocumentosDomainModel documentosDM = new DocumentosDomainModel();
+
+                string nombre = SessionPersister.AccountSession.NombreCompleto;
+                int idPersonal = SessionPersister.AccountSession.IdPersonal;
+                int idStatus = int.Parse(Recursos.RecursosSistema.REGISTRO_ESTADIA_EMPRESA);
+
+                estadiaEmpresaVM.idPersonal = idPersonal;
+                estadiaEmpresaVM.idStatus = idStatus;
+
+                AutoMapper.Mapper.Map(estadiaEmpresaVM,estadiaEmpresaDM);
+                AutoMapper.Mapper.Map(estadiaEmpresaVM.documentosVM,documentosDM);
+                estadiaEmpresaDM.documentosDM = documentosDM;
+
+                if (GuadarArchivo(estadiaEmpresaDM,nombre))
+                {
+                    estadiaEmpresaDM.documentosDM.StrUrl = estadiaEmpresaDM.documentosDM.DocumentoFile.FileName;
+                    DocumentosDomainModel documentos = documentosBusiness.AddDocumento(documentosDM);
+                    estadiaEmpresaDM.idDocumento = documentos.IdDocumento;
+                    estadiaEmpresaBusiness.AddUpdateEstadiaEmpresa(estadiaEmpresaDM);
+                    progresoProdepDM.idPersonal = idPersonal;
+                    progresoProdepDM.idStatus = idStatus;
+                    progresoProdep.AddUpdateProgresoProdep(progresoProdepDM);
+                }
+            }
+
+            return RedirectToAction("Create","EstadiaEmpresa");
+        }
+
+        public bool GuadarArchivo(EstadiaEmpresaDomainModel estadiaEmpresaDM, string nombre)
+        {
+            bool respuesta = false;
+
+            string path = Path.Combine(Server.MapPath(Recursos.RecursosSistema.DOCUMENTO_USUARIO + nombre + "/"));
+
+            if (Directory.Exists(path))
+            {
+                if (estadiaEmpresaDM.documentosDM.DocumentoFile.ContentType.Equals("application/pdf"))
+                {
+                    string pahtCompleto = Path.Combine(path, Path.GetFileName(estadiaEmpresaDM.documentosDM.DocumentoFile.FileName));
+                    estadiaEmpresaDM.documentosDM.DocumentoFile.SaveAs(pahtCompleto);
+                    respuesta = true;
+                }
+            }
+            else
+            {
+                DirectoryInfo directoryInfo = Directory.CreateDirectory(path);
+                GuadarArchivo(estadiaEmpresaDM, nombre);
+                respuesta = true;
+            }
+            return respuesta;
         }
     }
 }
--- AppDigitalCv/AppDigitalCv/Recursos/RecursosSistema.Designer.cs
+++ AppDigitalCv/AppDigitalCv/Recursos/RecursosSistema.Designer.cs
@@ -19,7 +19,7 @@ namespace AppDigitalCv.Recursos {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class RecursosSistema {
@@ -138,6 +138,15 @@ namespace AppDigitalCv.Recursos {
         internal static string REGISTRO_DIRECCION_INDIVIDUALIZADA {
             get {
                 return ResourceManager.GetString("REGISTRO_DIRECCION_INDIVIDUALIZADA", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a 11.
+        /// </summary>
+        internal static string REGISTRO_ESTADIA_EMPRESA {
+            get {
+                return ResourceManager.GetString("REGISTRO_ESTADIA_EMPRESA", resourceCulture);
             }
         }
         
