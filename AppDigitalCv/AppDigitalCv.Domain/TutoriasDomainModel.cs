d7b716c85073bdd2bc89e30d613f3538d24930fa
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-24 13:25:59 -0500

    Se Agro el Repositorio, VM y DomainModel de la entidad tblTutorias

---
+++ AppDigitalCv/AppDigitalCv.Domain/TutoriasDomainModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Domain
+{
+    public class TutoriasDomainModel
+    {
+        public int id { get; set; }
+        public int idPersonal { get; set; }
+        public int idStatus { get; set; }
+        public int idTipoEstudio { get; set; }
+        public int idProgramaEducativo { get; set; }
+        public string strTutoria { get; set; }
+        public string strNumeroEstudiantes { get; set; }
+        public string strNombreEstudantes { get; set; }
+        public DateTime dteFechaInicio { get; set; }
+        public DateTime dteFechaTermino { get; set; }
+        public string strTipo { get; set; }
+        public string strEstadoTutoria { get; set; }
+    }
+}
---
+++ AppDigitalCv/AppDigitalCv/ViewModels/TutoriasVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDigitalCv.Views
+{
+    public class TutoriasVM
+    {
+        public int id { get; set; }
+        public int idPersonal { get; set; }
+        public int idStatus { get; set; }
+        public int idTipoEstudio { get; set; }
+        public int idProgramaEducativo { get; set; }
+        public string strTutoria { get; set; }
+        public string strNumeroEstudiantes { get; set; }
+        public string strNombreEstudantes { get; set; }
+        public DateTime dteFechaInicio { get; set; }
+        public DateTime dteFechaTermino { get; set; }
+        public string strTipo { get; set; }
+        public string strEstadoTutoria { get; set; }
+    }
+}
