fbc748c3dd84f7ab56a5335d1da2abac303bb18f
fazur estrada ponce <limon-amargoverde15@hotmail.com>
2019-07-25 10:08:11 -0500

    Se agrego el Business y la interfas de la entidad tblTutoria

---
+++ AppDigitalCv/AppDigitalCv.Business/TutoriasBusiness.cs
@@ -0,0 +1,25 @@
+﻿using AppDigitalCv.Business.Interface;
+using AppDigitalCv.Repository;
+using AppDigitalCv.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDigitalCv.Business
+{
+    public class TutoriasBusiness : ITutoriasBusiness
+    {
+        private readonly IUnitOfWork unitofWork;
+        private readonly TutoriasRepository tutoriaRepository;
+
+        public TutoriasBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitofWork = _unitOfWork;
+            tutoriaRepository = new TutoriasRepository(unitofWork);
+        }
+
+        
+    }
+}
